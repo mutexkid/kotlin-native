af826f115463bd14fb193a69961cb8a62803e7f1
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2018-06-26 11:08:17 +0700

    LLVM API-based codegen

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
@@ -158,7 +158,7 @@ internal fun emitLLVM(context: Context, phaser: PhaseManager) {
         DFGSerializer.serialize(context, moduleDFG!!)
     }
 
-    phaser.phase(KonanPhase.CODEGEN) {
+    phaser.phase(KonanPhase.BITCODE_GENERATION) {
         irModule.acceptVoid(codegenVisitor)
     }
 
@@ -167,7 +167,7 @@ internal fun emitLLVM(context: Context, phaser: PhaseManager) {
     }
 }
 
-internal fun verifyModule(llvmModule: LLVMModuleRef, current: String = "") {
+internal fun verifyModule(llvmModule: LLVMModuleRef, context: Context, current: String = "") {
     memScoped {
         val errorRef = allocPointerTo<ByteVar>()
         // TODO: use LLVMDisposeMessage() on errorRef, once possible in interop.
@@ -175,7 +175,7 @@ internal fun verifyModule(llvmModule: LLVMModuleRef, current: String = "") {
                 llvmModule, LLVMVerifierFailureAction.LLVMPrintMessageAction, errorRef.ptr) == 1) {
             if (current.isNotEmpty())
                 println("Error in $current")
-            LLVMDumpModule(llvmModule)
+            LLVMWriteBitcodeToFile(llvmModule, context.config.tempFiles.create("error_dump", ".bc").path)
             throw Error("Invalid module")
         }
     }
@@ -747,7 +747,7 @@ internal class CodeGeneratorVisitor(val context: Context, val lifetimes: Map<IrE
         }
 
         if (context.shouldVerifyBitCode())
-            verifyModule(context.llvmModule!!,
+            verifyModule(context.llvmModule!!, context,
                 "${declaration.descriptor.containingDeclaration}::${ir2string(declaration)}")
     }
 
